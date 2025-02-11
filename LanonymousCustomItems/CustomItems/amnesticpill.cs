b6066276b7c6b03b4834cefa65431b399ca1d771
apb11 <apbblox11@gmail.com>
2025-02-11 12:34:45 -0500

    Update and rename ExplosiveBelt.cs to amnesticpill.cs

---
+++ LanonymousCustomItems/CustomItems/amnesticpill.cs
@@ -0,0 +1,74 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="CyanidePills.cs" company="Lanonymous">
+// Copyright (c) Lanonymous. All rights reserved.
+// Licensed under the CC BY-SA 4.0 license.
+// -----------------------------------------------------------------------
+
+
+using System.Collections.Generic;
+using Exiled.API.Enums;
+using Exiled.API.Features;
+using Exiled.API.Features.Attributes;
+using Exiled.API.Features.Spawn;
+using Exiled.CustomItems.API.Features;
+using Exiled.Events.EventArgs.Player;
+using MEC;
+using Player = Exiled.Events.Handlers.Player;
+
+
+namespace LanonymousCustomItems.CustomItems;
+
+public class amnesticpill
+{
+    [CustomItem(ItemType.Painkillers)]
+    public class amnestics : CustomItem
+    {
+        public ItemType ItemType { get; set; } = ItemType.Painkillers;
+            
+        public override uint Id { get; set; } = 101;
+            
+        public override string Name { get; set; } = "amnestic pills";
+            
+        public override string Description { get; set; } = "A cyanide pill.";
+            
+        public override float Weight { get; set; } = 0.5f;
+            
+        public override SpawnProperties SpawnProperties { get; set; } = new()
+        {
+            Limit = 1,
+            DynamicSpawnPoints = new List<DynamicSpawnPoint>
+            {
+                new()
+                {
+                    Chance = 100,
+                    Location = SpawnLocationType.InsideHczArmory,
+                },
+            },
+        };
+                
+        protected override void SubscribeEvents()
+        {
+            Player.UsingItem += OnUsingItem;
+            base.SubscribeEvents();
+        }
+
+        protected override void UnsubscribeEvents()
+        {
+            Player.UsingItem -= OnUsingItem;
+            base.UnsubscribeEvents();
+        }
+            
+        private void OnUsingItem(UsingItemEventArgs ev)
+        {
+            if (!Check(ev.Player.CurrentItem))
+                return;
+            ev.Player.EnableEffect(EffectType.Blurred,10, 10f, true);
+                
+            Timing.CallDelayed(10f, () => 
+            {
+                ev.Player.Kill(LanonymousCustomItems.Instance.Translation.CyanidePillDeathMessage);
+                Log.Debug($"{ev.Player.Nickname} has killed himself with Cyanide Pill");
+            });
+        }
+    }
+}
